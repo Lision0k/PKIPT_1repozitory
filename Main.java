eb9dc2771855ef34bbbe437feee235f8a205d6e0
Lision0k <ya.lizi.pav@gmail.com>
2025-02-28 12:18:21 +0300

    Сeating arrays with specifying steps and intervals and operations with them

--- Main.java
+++ Main.java
@@ -1,53 +1,76 @@
-import static java.lang.System.*;
-
-class Express {
-    float x;
-    float a;
-    float b;
-
-    Express(float x) {
-        this.x = x;
-    }
-
-    Express(float a, float b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    Express(float x, float a, float b) {
-        this.x = x;
-        this.a = a;
-        this.b = b;
-    }
-
-    public float chet1() {
-        return 3 * this.x + 5;
-    }
-
-    public String chet2() {
-        if ((this.a - this.b) != 0) {
-            String rez = String.valueOf((this.a + this.b) / (this.a - this.b));
-            return rez;
-        }
-        return "Error";
-    }
-
-    public String chet3() {
-        if (this.b != 0) {
-            String rez = String.valueOf((this.a * this.x) / this.b);
-            return rez;
-        }
-        return "Error";
-    }
-}
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Express X = new Express(2.1f);
-        out.println(X.chet1());
-        Express Y = new Express(2.1f, 1.1f);
-        out.println(Y.chet2());
-        Express Z = new Express(2.1f, 1.2f, 2.0f);
-        out.println(Z.chet3());
+        double[] B = Vvod(); //R2
+        System.out.println("R2 = " + Arrays.toString(B));
+        double[] C = Vvod();
+        double[] R1 = Vvod();
+        System.out.println("R1 = " + Arrays.toString(R1));
+        double[] X = Vvod();
+        System.out.println("Rx = " + Arrays.toString(X));
+        double[] A = Ymn(R1, B);
+        double[] A1 = new double[A.length];
+        Sum(new double[]{1.0}, A, A1);
+        double[] Z1 = new double[A.length*B.length*X.length];
+        double[] Z2 = new double[R1.length*R1.length*X.length];
+        double[] Z = new double[Z1.length * Z2.length];
+        Sum(Ymn(A1, X), B, Z1);
+        Sum(Ymn(R1, X), R1, Z2);
+        Del(Z1, Z2, Z);
+        for (double i : X)
+            for (double j : Z)
+                if (i == j)
+                    System.out.print(i + ", ");
+    }
+
+    static double[] Vvod(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите шаг: ");
+        double shag = in.nextDouble();
+        System.out.print("Введите начало диапазона: ");
+        double start = in.nextDouble();
+        System.out.print("Введите конец диапазона: ");
+        double end = in.nextDouble();
+        int sh = 0;
+        for (double i = start; i <= end; i += shag)
+            sh++;
+        double[] A = new double[sh];
+        sh = 0;
+        for (double i = start; i <= end; i += shag){
+            A[sh] = i;
+            sh++;
+        }
+        return A;
+    }
+
+    static void Sum(double[] A, double[] B, double[] Z){
+        int sh = 0;
+        for (double i : A) {
+            for (double j : B) {
+                Z[sh] = i + j;
+                sh++;
+            }
+        }
+    }
+    static void Del(double[] A, double[] B, double[] Z){
+        int sh = 0;
+        for (double i : A) {
+            for (double j : B) {
+                Z[sh] = i / j;
+                sh++;
+            }
+        }
+    }
+    static double[] Ymn(double[] A, double[] B){
+        double[] Z = new double[A.length*B.length];
+        int sh = 0;
+        for (double i : A) {
+            for (double j : B) {
+                Z[sh] = i * j;
+                sh++;
+            }
+        }
+        return Z;
     }
 }
