689ad7e1c808695b018a99af097723d379d2a32a
Lision0k <ya.lizi.pav@gmail.com>
2025-02-28 12:16:08 +0300

    Practical work № 5

--- Main.java
+++ Main.java
@@ -1,77 +1,53 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import static java.lang.System.*;
+
+class Express {
+    float x;
+    float a;
+    float b;
+
+    Express(float x) {
+        this.x = x;
+    }
+
+    Express(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    Express(float x, float a, float b) {
+        this.x = x;
+        this.a = a;
+        this.b = b;
+    }
+
+    public float chet1() {
+        return 3 * this.x + 5;
+    }
+
+    public String chet2() {
+        if ((this.a - this.b) != 0) {
+            String rez = String.valueOf((this.a + this.b) / (this.a - this.b));
+            return rez;
+        }
+        return "Error";
+    }
+
+    public String chet3() {
+        if (this.b != 0) {
+            String rez = String.valueOf((this.a * this.x) / this.b);
+            return rez;
+        }
+        return "Error";
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        double[] A = Vvod();
-        System.out.println("A = " + Arrays.toString(A));
-        double[] B = Vvod();
-        System.out.println("B = " + Arrays.toString(B));
-        double[] C = Vvod();
-        System.out.println("C = " + Arrays.toString(C));
-        double[] D = Vvod();
-        System.out.println("D = " + Arrays.toString(D));
-        double[] X = Vvod();
-        System.out.println("X = " + Arrays.toString(X));
-        double[] Z1 = new double[A.length*B.length*X.length];
-        double[] Z2 = new double[C.length*D.length*X.length];
-        double[] Z = new double[Z1.length * Z2.length];
-        Sum(Ymn(A, X), B, Z1);
-        Sum(Ymn(C, X), D, Z2);
-        Del(Z1, Z2, Z);
-        for (double i : X)
-            for (double j : Z)
-                if (i == j)
-                    System.out.print(i + ", ");
+        Express X = new Express(2.1f);
+        out.println(X.chet1());
+        Express Y = new Express(2.1f, 1.1f);
+        out.println(Y.chet2());
+        Express Z = new Express(2.1f, 1.2f, 2.0f);
+        out.println(Z.chet3());
     }
-
-    static double[] Vvod(){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите шаг: ");
-        double shag = in.nextDouble();
-        System.out.print("Введите начало диапазона: ");
-        double start = in.nextDouble();
-        System.out.print("Введите конец диапазона: ");
-        double end = in.nextDouble();
-        int sh = 0;
-        for (double i = start; i <= end; i += shag)
-            sh++;
-        double[] A = new double[sh];
-        sh = 0;
-        for (double i = start; i <= end; i += shag){
-            A[sh] = i;
-            sh++;
-        }
-        return A;
-    }
-
-    static void Sum(double[] A, double[] B, double[] Z){
-        int sh = 0;
-        for (double i : A) {
-            for (double j : B) {
-                Z[sh] = i + j;
-                sh++;
-            }
-        }
-    }
-    static void Del(double[] A, double[] B, double[] Z){
-        int sh = 0;
-        for (double i : A) {
-            for (double j : B) {
-                Z[sh] = i / j;
-                sh++;
-            }
-        }
-    }
-    static double[] Ymn(double[] A, double[] B){
-        double[] Z = new double[A.length*B.length];
-        int sh = 0;
-        for (double i : A) {
-            for (double j : B) {
-                Z[sh] = i * j;
-                sh++;
-            }
-        }
-        return Z;
-    }
-
 }
